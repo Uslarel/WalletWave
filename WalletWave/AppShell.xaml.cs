bc3da66778253cadc0203074781aa62fa7c64471
Ilya Verashchaka <archizaken@gmail.com>
2023-12-26 22:34:15 +0100

    feat: Change from blazor to native way.

---
+++ WalletWave/AppShell.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace WalletWave;
+
+public partial class AppShell : Shell
+{
+    public AppShell() { InitializeComponent(); }
+}
--- WalletWave/Platforms/iOS/AppDelegate.cs
+++ WalletWave/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace WalletWave;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
